d47367d156749c291cb866b373931b98cb8ead86
Victor Anolu <afactor05@gmail.com>
2025-12-30 11:23:02 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -123,7 +123,7 @@ hcloud CAE ExecuteAction \
   --spec.source.code.auth_name="$AUTH_NAME" \
   --spec.source.code.namespace="$NAMESPACE" \
   --spec.build.parameters.dockerfile_path="$DOCKERFILE_PATH"
-  --spec.build.archive.artifact_namespace="$ARTIFACT_NAMESPACE"
+  --spec.build.archive.artifact_namespace="secureid_digital_repos"
 
 echo "Deployment successful."
 
